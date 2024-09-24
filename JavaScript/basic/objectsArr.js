afa4cb11864d9b6e397a185c72cb51a04082a163
samar <samarmohd251@gmail.com>
2024-09-24 23:53:13 +0530

    changes

---
+++ JavaScript/basic/objectsArr.js
@@ -0,0 +1,8 @@
+// reference 
+const person1={name:`john`,age:20};
+console.log(`person1`,person1);
+const person2=person1;
+person2.age=30;
+console.log(`person2`,person2);
+console.log(`person1`,person1);
+
