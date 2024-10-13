86e8e0992de286686ddad59c93a5d822e086079a
samar <samarmohd251@gmail.com>
2024-10-13 22:58:36 +0530

    add

---
+++ JavaScript/basic/Dom/getElementByID.js
@@ -0,0 +1,25 @@
+// ! selecting element by ID
+const conferenceTitleEl=document.getElementById(`conferenceTitle`);
+console.log(conferenceTitleEl);
+console.log(conferenceTitleEl.innerHTML);
+
+const conferenceDateEl=document.getElementById(`conferenceDate`);
+console.log(conferenceDateEl);
+
+const locationTitleEl=document.getElementById(`locationTitle`);
+console.log(locationTitleEl);
+
+const conferenceLocationEl=document.getElementById(`conferenceLocation`);
+console.log(conferenceLocationEl);
+
+const speakersTitleEl=document.getElementById(`speakersTitle`);
+console.log(speakersTitleEl);
+
+const speakerListEl=document.getElementById(`speakerList`);
+console.log(speakerListEl );
+
+console.log({conferenceDateEl,conferenceLocationEl,conferenceTitleEl,locationTitleEl,speakerListEl,speakersTitleEl});
+
+
+
+
