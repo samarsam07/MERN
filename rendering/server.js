22e0057f0a0a00d51b2ab5023a06b6469dea8f6e
samar <samarmohd251@gmail.com>
2024-12-28 19:19:03 +0530

    adding new files

--- rendering/server.js
+++ rendering/server.js
@@ -1,25 +1,62 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 8082;
 
 app.use(express.static(path.join(__dirname, "public")));
+// set view engine
+app.set("view engine", "ejs");
 
 //home page
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "home.html"));
+  res.render("home.ejs");
 });
 // about page
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "about.html"));
+  // res.sendFile(path.join(__dirname, "view", "about.html"));
+  res.render("about.ejs");
 });
 // contact page
 app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "contact.html"));
+  // res.sendFile(path.join(__dirname, "view", "contact.html"));
+  res.render("contact.ejs");
 });
 // gallery page
 app.get("/gallery", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "gallery.html"));
+  // res.sendFile(path.join(__dirname, "view", "gallery.html"));
+  res.render("gallery.ejs");
+});
+// rendering userdata
+app.get("/userdata", (req, res) => {
+  // res.sendFile(path.join(__dirname, "view", "gallery.html"));
+  // dummy user data
+  const userData = {
+    username: "Alice",
+    age: 25,
+    isPermitted: true,
+    email: "alice123@gmail.com",
+    islogin: false,
+  };
+  res.render("userData.ejs", userData);
+});
+// render products
+app.get("/products", (req, res) => {
+  // dummy products
+  const products = [
+    {
+      name: "laptop",
+      price: 999,
+    },
+    {
+      name: "phone",
+      price: 500,
+    },
+    {
+      name: "TV",
+      price: 800,
+    },
+  ];
+  res.render("products.ejs", {products});
 });
 // start the server
 
